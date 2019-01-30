888d23080ee63de8ee4829385d02ffca0cef8219
Christoph Gruener <christophgruener@me.com>
2019-01-30 21:31:43 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,7 @@
  }
 
 def select_winner(passengers)
-   
-end
+   winner = ""
+   passengers.each { |suite, name| if suite == :suite_a && name.start_with?("A") then winner = name end}
+   winner
+end 
